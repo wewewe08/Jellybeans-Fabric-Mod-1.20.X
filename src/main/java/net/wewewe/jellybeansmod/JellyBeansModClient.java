2b6e3034909e47dce6abf0bce3f36ec6cccb03ee
yvonne <yvonnehuang0810@gmail.com>
2023-09-19 18:49:30 -0400

    Updated empty jar textures

--- src/main/java/net/wewewe/jellybeansmod/JellyBeansModClient.java
+++ src/main/java/net/wewewe/jellybeansmod/JellyBeansModClient.java
@@ -1,10 +1,13 @@
 package net.wewewe.jellybeansmod;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.minecraft.client.render.RenderLayer;
+import net.wewewe.jellybeansmod.block.ModBlocks;
 
 public class JellyBeansModClient implements ClientModInitializer {
     @Override
     public void onInitializeClient(){
-
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.EMPTY_JAR, RenderLayer.getCutout());
     }
 }
